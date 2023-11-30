130a30773ed08446eaf0cf4f156f19217de0b304
aDHIxx <sreemohan.adhithya@gmail.com>
2023-12-01 01:08:33 +0530

    Added: UseCase 2: Creating linkedlist by adding 30 and 56 to 70

--- linkedListGenerics/src/com/bridgelabz/linkedlist/Main.java
+++ linkedListGenerics/src/com/bridgelabz/linkedlist/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         // Creating a simple linked list with values 56, 30, and 70
         LinkedList<Integer> linkedList = new LinkedList<>();
-        linkedList.add(56);
-        linkedList.add(30);
         linkedList.add(70);
+        linkedList.add(30);
+        linkedList.add(56);
 
         // Displaying the elements of the linked list
         linkedList.display();
